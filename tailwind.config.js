53e2e3723dd7a78775f5b62945b74000dc68aa0d
koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2024-08-27 15:07:54 +0530

    🔧 Add configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
